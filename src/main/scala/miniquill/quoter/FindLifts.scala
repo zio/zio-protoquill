dc2ec3d4e80ad49b778b9a6b81bf864a5ad99b5f
deusaquilus <deusaquilus@gmail.com>
2020-02-25 00:26:54 -0500

    Refactor ExprAccumulate, enable some tests

--- src/main/scala/miniquill/quoter/FindLifts.scala
+++ src/main/scala/miniquill/quoter/FindLifts.scala
@@ -30,33 +30,7 @@ object FindEncodeables {
   }
 }
 
-object ExprAccumulate {
-  def apply[T](input: Expr[Any])(matcher: PartialFunction[Expr[Any], T])(given qctx: QuoteContext): List[T] = {
-    import qctx.tasty.{Type => QType, given, _}
 
-    val buff: ArrayBuffer[T] = new ArrayBuffer[T]()
-    val accum = new ExprMap {
-      def transform[TF](expr: Expr[TF])(given qctx: QuoteContext, tpe: Type[TF]): Expr[TF] = {
-        matcher.lift(expr) match {
-          case Some(result) => 
-            buff += result
-            expr
-          case None =>
-            expr
-        }
-
-        expr.unseal match {
-          // Not including this causes execption "scala.tasty.reflect.ExprCastError: Expr: [ : Nothing]" in certain situations
-          case Repeated(Nil, Inferred()) => expr 
-          case _ => transformChildren[TF](expr)
-        }
-      }
-    }
-
-    accum.transform(input)
-    buff.toList
-  }
-}
 
 object FindLifts {
 
