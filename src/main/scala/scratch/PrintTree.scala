12f4ce5c6c6b4a2f4f1ad75b22b354fe9c78a035
deusaquilus <deusaquilus@gmail.com>
2020-04-29 00:36:30 -0400

    Initial version

---
+++ src/main/scala/scratch/PrintTree.scala
@@ -0,0 +1,14 @@
+package scratch
+
+import scala.quoted._
+
+object printTree {
+  inline def apply[T](inline tree: T): T = ${ printTreeImpl[T]('tree) }
+  def printTreeImpl[T:Type](tree: Expr[T])(given qctx: QuoteContext): Expr[T] = {
+    import qctx.tasty.{given _, _}
+    println(tree.show)
+    printer.lnf(tree.unseal)
+    printer.lnf(tree.unseal.underlyingArgument)
+    tree
+  }
+}
