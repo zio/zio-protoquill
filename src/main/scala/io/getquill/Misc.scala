ea70b6e8223e2c75eabc3a220a21c4ced4027b17
deusaquilus <deusaquilus@gmail.com>
2020-11-17 02:14:35 -0500

    Example of how to overcome the double-definition error

--- src/main/scala/io/getquill/Misc.scala
+++ src/main/scala/io/getquill/Misc.scala
@@ -1,1 +1,22 @@
 package io.getquill
+
+import scala.quoted._
+
+/**
+ * Adding the stringDummy and intDummy ensures that a Double definition error
+ * does not occur.
+ */
+object Misc {
+  trait StringDummy
+  trait IntDummy
+  implicit val stringDummy: StringDummy = new StringDummy {}
+  implicit val intDummy: IntDummy = new IntDummy {}
+
+  inline def foo(inline a: List[String])(implicit s: StringDummy): Unit = ${ fooImpl('a) }
+  def fooImpl(a: Expr[List[String]])(implicit qctx: QuoteContext): Expr[Unit] = {
+    println(pprint.apply(a.unseal))
+    '{ () }
+  }
+
+  inline def foo(b: List[Int])(implicit i: IntDummy) = println(b)
+}
---
+++ src/main/scala/io/getquill/MiscRun.scala
@@ -0,0 +1,10 @@
+package io.getquill
+
+import Misc._
+
+object MiscRun {
+  def main(args: Array[String]):Unit = {
+    foo(List("a","b","c"))
+    foo(List(1,2,3))
+  }
+}
