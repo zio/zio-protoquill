0b7d2cec71c7562cf170a92691ad740e205dc224
deusaquilus <deusaquilus@gmail.com>
2020-11-19 02:32:40 -0500

    change example to idiomatic

--- src/main/scala/miniquill/InlineMacroTest3InlineMacroTest5_Typelevel.scala
+++ src/main/scala/miniquill/InlineMacroTest3InlineMacroTest5_Typelevel.scala
@@ -13,24 +13,24 @@ object InlineMacroTest5_Typelevel {
   val ctx = new MirrorContext(MirrorSqlDialect, Literal)
   import ctx._
 
-  trait Conv[In] {
+  trait Conv[In]:
     type Out
     inline def get(inline i: In): Out
-  }
-  class ConvString extends Conv[String] {
+  
+  class ConvString extends Conv[String]:
     type Out = Int
     inline def get(inline i: String): Int = i.length + 55
-  }
-  class ConvInt extends Conv[Int] {
+  
+  class ConvInt extends Conv[Int]:
     type Out = String
     inline def get(inline t: Int): String = t.toString + "suffix1"
-  }
-  implicit def convString: ConvString = new ConvString
-  implicit def convInt: ConvInt = new ConvInt
+  
+  inline given convString as ConvString = new ConvString
+  inline given convInt as ConvInt = new ConvInt
 
-  inline def conv[T](inline element: T)(implicit inline conv: Conv[T]): conv.Out = {
+  inline def conv[T](inline element: T)(using inline conv: Conv[T]): conv.Out =
     conv.get(element)
-  }
+  
   inline def q1 = quote { query[Person].map(p => conv(p.name) + 66) }
   println(run(q1))
 
