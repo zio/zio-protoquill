0189e3fe0baf9eec7fd4fb06e7d346011fc34d9d
deusaquilus <deusaquilus@gmail.com>
2020-11-22 13:36:05 -0500

    Continue using

--- src/main/scala/miniquill/parser/MyParserUse.scala
+++ src/main/scala/miniquill/parser/MyParserUse.scala
@@ -2,8 +2,16 @@ package miniquill.parser
 
 object MyParserUse {
   def main(args: Array[String]):Unit = {
-    inline def tup = ("foo", "bar")
+    //inline def tup = ("foo", "bar")
     //PrintMac(tup)
-    MatchMac(tup)
+
+    // Something like this should be possible in quill, looks like a proxy-val is generated
+    // could the regular val-parser do that?
+    inline def fun = (a: Int, b: String) => a.toString + b
+    inline def pass(tup: (Int, String) => String): String = tup(1, "blah")
+
+    println(pass(fun))
+
+    PrintMac(pass(fun))
   }
 }
