9b7442cebe9f5b13fff25f984bed5ad76199cad7
Alexander Ioffe <deusaquilus@gmail.com>
2020-12-13 04:32:11 -0500

    Simplify Test

--- src/test/scala/io/getquill/QueryTest.scala
+++ src/test/scala/io/getquill/QueryTest.scala
@@ -11,43 +11,16 @@ import miniquill.quoter.QuotationVase
 import io.getquill.context.ExecutionType
 import org.scalatest._
 
-class QueryTest extends Spec with Inside { //hellooooooo
+class QueryTest {
 
-  // TODO Need to test 3-level injection etc...
-  case class Address(street:String, zip:Int) extends Embedded
-  case class Person(name: String, age: Int, address: Address)
-  val sqlCtx = new MirrorContext(MirrorSqlDialect, Literal)
+  case class Person(name: String, age: Int)
   val ctx = new MirrorContext(MirrorIdiom, Literal)
 
-  inline def people = quote {
-    query[Person]
-  }
-  inline def addresses = quote {
-    people.map(p => p.address)
-  }
   def peopleRuntime = quote {
     query[Person]
   }
-  // TODO Need join semantics to test these (also add id, fk to case classes)
-  // def addressesRuntimePeopleCompiletime = quote {
-  //  peopleRuntime.join(address).on(_.name == _.street)
-  // }
-  // def addressesCompiletimePeopleRuntime = quote {
-  //  peopleRuntime.join(addressRuntime).on(_.name == _.street)
-  // }
-  def addressesRuntime = quote {
-    peopleRuntime.map(p => p.address)
-  }
-
-
-  // one level object query
-  // @Test
-  def oneLevelQuery(): Unit = {
-    
-  }
-
-  "shuold work correctly with lift" in {
-    import ctx._
+  
+  def main(args: Array[String]): Unit = {
     println( ctx.runAndTest(  peopleRuntime.map(p => p.name)) ) //helloooooooooooooooooooooooooooo
   }
 
