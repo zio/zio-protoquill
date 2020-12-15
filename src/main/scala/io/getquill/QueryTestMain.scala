eb659317a67d897995360d7fb3ebf4492b7b6124
Alexander Ioffe <deusaquilus@gmail.com>
2020-12-15 04:02:34 -0500

    Fix an import

--- src/main/scala/io/getquill/QueryTestMain.scala
+++ src/main/scala/io/getquill/QueryTestMain.scala
@@ -4,12 +4,10 @@ import scala.language.implicitConversions
 import miniquill.quoter.Dsl._
 import miniquill.quoter.Quoted
 import miniquill.quoter._
-import io.getquill._
 import io.getquill.ast._
 import miniquill.quoter.QuotationLot
 import miniquill.quoter.QuotationVase
 import io.getquill.context.ExecutionType
-import org.scalatest._
 
 class QueryTestMain {
 
