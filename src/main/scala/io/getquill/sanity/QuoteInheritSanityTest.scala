50d3b6d0f2325402b6af40c6f84df364859b6a00
Alexander Ioffe <deusaquilus@gmail.com>
2020-12-23 20:06:48 -0500

    More cleanup

--- src/main/scala/io/getquill/sanity/QuoteInheritSanityTest.scala
+++
@@ -1,7 +0,0 @@
-package io.getquill.sanity
-
-@main def sanityTest() = {
-  inline def foo = "one"
-  inline def bar = foo
-  println( CompileTimeTree.printUnderlyingArg(bar) )
-}
--- src/main/scala/scratch/TraitMatchTest.scala
+++
@@ -1,7 +0,0 @@
-// package scratch
-
-// object TraitMatchTest {
-//   def main(args: Array[String]): Unit = {
-//     TraitMatch.traitMatch(Impl(1, "one")) //hellooo
-//   }
-// }
