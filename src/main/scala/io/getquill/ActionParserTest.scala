47fc5781a7de8fe9fc3df738d6464f722234ca00
Prakhar Saxena <prakharsaxena914@gmail.com>
2021-02-11 23:44:00 -0500

    added context to generate the queries

--- src/main/scala/io/getquill/ActionParserTest.scala
+++ src/main/scala/io/getquill/ActionParserTest.scala
@@ -8,37 +8,43 @@ object ActionParserTest {
   def main(args: Array[String]):Unit = {
     println('\n'*10)
 
+    val ctx = new MirrorContext(MirrorSqlDialect, Literal)
+    import ctx._
+
     case class Person(name:String, age:Int)
 
     val insertOutput = quote {
       query[Person].insert(_.name -> "John", _.age -> 21)
       //INSERT INTO Person (name,age) VALUES (?, ?)
     }
-    run(insertOutput)
+    println()
+    ctx.run(insertOutput)
 
     val updateOutput = quote {
       query[Person].filter(_.name=="Joe").update(_.name -> "John")
        //UPDATE Person SET name = ? WHERE name = ?
     }
-    run(updateOutput)
+    println()
+    ctx.run(updateOutput)
 
     inline def deleteOutput = quote {
       query[Person].filter(p => p.name == "Joe").delete
       //DELTE FROM Person WHERE name = 'Joe'
     }
-    run(deleteOutput)
+    println()
+    ctx.run(deleteOutput)
 
     // val queryReturningOutput = quote {
     //   query[Person].insert(_.name -> "John", _.age -> 21).returning(p => p.name)
     //   //INSERT INTO Person (name, age) VALUES ('Joe', 21) RETURNING name
     // }
-    // run(queryReturningOutput)
+    // ctx.run(queryReturningOutput)
 
     // val queryReturningGeneratedOutput = quote {
     //   query[Person].insert(_.name -> "John", _.age -> 21).returningGenerated(p => p.name)
     //   //INSERT INTO Person (id, name, age) VALUES (-1, 'Joe', 1) RETURNING id
     // }
-    // run(queryReturningGeneratedOutput)
+    // ctx.run(queryReturningGeneratedOutput)
     
     //compileeeeeeeee
 
