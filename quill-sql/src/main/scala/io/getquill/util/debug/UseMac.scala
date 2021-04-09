f1b347f647ab684f382aa814f29cf9ac9fcf6961
Alexander Ioffe <deusaquilus@gmail.com>
2021-04-08 22:42:29 -0400

    Intermittentl failing when in source code. Not sure why. Commenting out for now

--- quill-sql/src/main/scala/io/getquill/util/debug/UseMac.scala
+++ quill-sql/src/main/scala/io/getquill/util/debug/UseMac.scala
@@ -1,31 +1,31 @@
-package io.getquill.util.debug
+// package io.getquill.util.debug
 
-import io.getquill._
-import io.getquill.Dsl._
+// import io.getquill._
+// import io.getquill.Dsl._
 
-object UseMac {
-  case class Person(name: String, age: Int)
-  val ctx = new MirrorContext(PostgresDialect, Literal)
-  import ctx._
+// object UseMac {
+//   case class Person(name: String, age: Int)
+//   val ctx = new MirrorContext(PostgresDialect, Literal)
+//   import ctx._
 
-  def main(args: Array[String]):Unit = {
-    val l = List(Person("Joe", 123))
-    // NOTE doing querySchema here directly does not seem to work in InsertMacro, need to further test that
-    //inline def q = quote { liftQuery(l).foreach(p => querySchema[Person]("thePerson").insert(p)) }
+//   def main(args: Array[String]):Unit = {
+//     val l = List(Person("Joe", 123))
+//     // NOTE doing querySchema here directly does not seem to work in InsertMacro, need to further test that
+//     //inline def q = quote { liftQuery(l).foreach(p => querySchema[Person]("thePerson").insert(p)) }
 
-    inline def q = quote { liftQuery(l).foreach(p => query[Person].insert(p)) }
+//     inline def q = quote { liftQuery(l).foreach(p => query[Person].insert(p)) }
 
-    //inline def q = liftQuery(l).foreach(p => query[Person].insert(p))
-    // import io.getquill.context.LiftMacro
-    //inline def content = LiftMacro.liftInjectedProductExternal[Person, Int]
-    //PrintMac(content)
+//     //inline def q = liftQuery(l).foreach(p => query[Person].insert(p))
+//     // import io.getquill.context.LiftMacro
+//     //inline def content = LiftMacro.liftInjectedProductExternal[Person, Int]
+//     //PrintMac(content)
 
-    //val list = LiftMacro.liftInjectedProductExternal[Person, Int]
-    //println( list.map(elem => (elem._1, elem._2.apply(Person("Joe", 123)))) )
+//     //val list = LiftMacro.liftInjectedProductExternal[Person, Int]
+//     //println( list.map(elem => (elem._1, elem._2.apply(Person("Joe", 123)))) )
 
-    //PrintMac(q) //hellooooooooooooooooooooooooooo
+//     //PrintMac(q) //hellooooooooooooooooooooooooooo
 
-    // Uncomment to get error
-    println( run(q) )
-  }
-}
+//     // Uncomment to get error
+//     println( run(q) )
+//   }
+// }
