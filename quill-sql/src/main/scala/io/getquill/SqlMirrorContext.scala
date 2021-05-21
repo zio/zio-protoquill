edb4028fbd0a37920d397000ad28d790116f68f9
Alexander Ioffe <deusaquilus@gmail.com>
2021-05-21 17:53:51 -0400

    Array encoding specs, also working with Jasync

--- quill-sql/src/main/scala/io/getquill/SqlMirrorContext.scala
+++ quill-sql/src/main/scala/io/getquill/SqlMirrorContext.scala
@@ -2,9 +2,9 @@ package io.getquill
 
 import io.getquill.idiom.{ Idiom => BaseIdiom }
 import io.getquill.context.sql.SqlContext
-//import io.getquill.context.sql.encoding.mirror.ArrayMirrorEncoding
+import io.getquill.context.mirror.ArrayMirrorEncoding
 
 class SqlMirrorContext[Idiom <: BaseIdiom, Naming <: NamingStrategy](idiom: Idiom, naming: Naming)
   extends MirrorContext(idiom, naming)
   with SqlContext[Idiom, Naming]
-  //with ArrayMirrorEncoding
+  with ArrayMirrorEncoding
