7cc92811c7267dca629df03fb3580d3fbadf9921
Alexander Ioffe <deusaquilus@gmail.com>
2021-05-31 22:13:22 -0400

    Cleaning up some printing stuff

---
+++ quill-sql/src/main/scala/io/getquill/util/debug/PrintMacExample.scala
@@ -0,0 +1,19 @@
+// package io.getquill.util.debug
+
+// import io.getquill._
+
+// object PrintMacExample {
+//   case class Person(name: String, age: Int)
+
+//   // This class needs to be in it's own compilation unit seperate from Parsing.scala etc...
+//   // While it can remain in the main codebase for convenience reasons, it has to be commented
+//   // in case files like Parsing.scala need to be recompiled
+//   def main(args: Array[String]): Unit = {
+//     val ctx = new SqlMirrorContext(PostgresDialect, Literal)
+//     import ctx._
+//     inline def q = quote {
+//       query[Person]
+//     }
+//     PrintMac(q)
+//   }
+// }
