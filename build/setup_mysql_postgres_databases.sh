ba8e7de1a771da5b0dc465c31199465514ff4088
Alexander Ioffe <deusaquilus@gmail.com>
2021-06-02 09:58:56 -0400

    Trying to get scripts working with images

--- build/setup_mysql_postgres_databases.sh
+++ build/setup_mysql_postgres_databases.sh
@@ -9,7 +9,7 @@ time docker-compose up -d postgres mysql
 . build/setup_db_scripts.sh
 
 # run setup scripts for local databases
-time setup_mysql $MYSQL_SCRIPT 127.0.0.1
-time setup_postgres $POSTGRES_SCRIPT 127.0.0.1
+time setup_mysql $MYSQL_SCRIPT 127.0.0.1:13306
+time setup_postgres $POSTGRES_SCRIPT 127.0.0.1:15432
 
 echo "Postgres and MySQL Databases are ready!"
