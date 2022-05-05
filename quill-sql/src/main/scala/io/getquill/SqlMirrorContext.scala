6c4f75d9a40804b17dd9182009b9ab8e5d8c0c93
Alexander Ioffe <deusaquilus@gmail.com>
2022-05-05 03:34:39 -0400

    Another IntelliJ (SCL-20185) Workaround

--- quill-sql/src/main/scala/io/getquill/SqlMirrorContext.scala
+++ quill-sql/src/main/scala/io/getquill/SqlMirrorContext.scala
@@ -3,8 +3,14 @@ package io.getquill
 import io.getquill.idiom.{Idiom => BaseIdiom}
 import io.getquill.context.sql.SqlContext
 import io.getquill.context.mirror.ArrayMirrorEncoding
+import io.getquill.context.AstSplicing
 
-class SqlMirrorContext[Idiom <: BaseIdiom, Naming <: NamingStrategy](idiom: Idiom, naming: Naming)
-    extends MirrorContext(idiom, naming)
+/** Workaround for IntelliJ SCL-20185. Inheriting MirrorContextBase directly so that `run` methods have autocomplete. */
+class SqlMirrorContext[Idiom <: BaseIdiom, Naming <: NamingStrategy](val idiom: Idiom, val naming: Naming)
+    extends MirrorContextBase[Idiom, Naming]
+    with AstSplicing
     with SqlContext[Idiom, Naming]
-    with ArrayMirrorEncoding
+    with ArrayMirrorEncoding:
+
+  val session: MirrorSession = MirrorSession("DefaultMirrorContextSession")
+end SqlMirrorContext
