d24e680339212a40e9c9be3ab29375ad7feccfe5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-09-21 19:51:53 -0400

    spelling: separate
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- quill-sql/src/main/scala/io/getquill/util/debug/PrintMacExample.scala
+++ quill-sql/src/main/scala/io/getquill/util/debug/PrintMacExample.scala
@@ -5,7 +5,7 @@
 // object PrintMacExample {
 //   case class Person(name: String, age: Int)
 
-//   // This class needs to be in it's own compilation unit seperate from Parsing.scala etc...
+//   // This class needs to be in it's own compilation unit separate from Parsing.scala etc...
 //   // While it can remain in the main codebase for convenience reasons, it has to be commented
 //   // in case files like Parsing.scala need to be recompiled
 //   def main(args: Array[String]): Unit = {
