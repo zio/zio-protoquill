efa619c5ce7a753931b35f28c102e87ffae1cfda
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-09-21 19:51:42 -0400

    spelling: particularize
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- quill-sql/src/main/scala/io/getquill/context/Unparticular.scala
+++ quill-sql/src/main/scala/io/getquill/context/Unparticular.scala
@@ -41,7 +41,7 @@ object Unparticular:
    * will look like this "WHERE p.name in (?)" (this is the basicQuery).
    * This last "?" actually needs to be expanded
    * into a comma separated list coming from the lifted list which is actually Expr[List[T]]
-   * but that will be done in the Particularizee(r). The `realQuery` is a tokenized representation
+   * but that will be done in the Particularize(r). The `realQuery` is a tokenized representation
    * of the query that can be turned into what it actually will need to look like by the
    * Particularize(r)
    */
