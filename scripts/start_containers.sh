4a5ddd14473da38d1e42bbcf84a53d3657582ecb
Juliano Alves <von.juliano@gmail.com>
2024-10-02 16:08:18 +0100

    fix build (#497)
    * fix build
    
    * bump scala version

--- scripts/start_containers.sh
+++ scripts/start_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # From 'All In One' of Quill CONTRIBUTING.md
-docker-compose down && docker-compose build && docker-compose run --rm --service-ports setup
+docker compose down && docker compose build && docker compose run --rm --service-ports setup
 
 # echo "Adding 50ms latency to protoquill_postgres_1"
 # docker exec protoquill_postgres_1 tc qdisc add dev eth0 root netem delay 50ms
